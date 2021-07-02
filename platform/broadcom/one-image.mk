e08892b2544078ac3bce92c85a937bbf72a03b06
akar <akarl@celestica.com>
2021-07-02 08:17:17 +0000

    syncup with sonic-buildimage(azure)202012

--- platform/broadcom/one-image.mk
+++ platform/broadcom/one-image.mk
@@ -35,6 +35,7 @@ $(SONIC_ONE_IMAGE)_LAZY_INSTALLS += $(DELL_S6000_PLATFORM_MODULE) \
                                $(ACCTON_AS5812_54T_PLATFORM_MODULE) \
                                $(ACCTON_AS5835_54X_PLATFORM_MODULE) \
                                $(ACCTON_AS9716_32D_PLATFORM_MODULE) \
+                               $(ACCTON_AS9726_32D_PLATFORM_MODULE) \
                                $(ACCTON_AS5835_54T_PLATFORM_MODULE) \
                                $(ACCTON_AS7312_54XS_PLATFORM_MODULE) \
                                $(ACCTON_AS7315_27XB_PLATFORM_MODULE) \
@@ -46,14 +47,15 @@ $(SONIC_ONE_IMAGE)_LAZY_INSTALLS += $(DELL_S6000_PLATFORM_MODULE) \
                                $(CEL_DX010_PLATFORM_MODULE) \
                                $(CEL_HALIBURTON_PLATFORM_MODULE) \
                                $(CEL_SEASTONE2_PLATFORM_MODULE) \
-                               $(CEL_BELGITE_PLATFORM_MODULE) \
                                $(DELTA_AG9032V1_PLATFORM_MODULE) \
                                $(DELTA_AG9064_PLATFORM_MODULE) \
                                $(DELTA_AG5648_PLATFORM_MODULE) \
                                $(DELTA_ET6248BRB_PLATFORM_MODULE) \
                                $(QUANTA_IX1B_32X_PLATFORM_MODULE) \
                                $(QUANTA_IX7_32X_PLATFORM_MODULE) \
+                               $(QUANTA_IX7_BWDE_32X_PLATFORM_MODULE) \
                                $(QUANTA_IX8_56X_PLATFORM_MODULE) \
+                               $(QUANTA_IX8A_BWDE_56X_PLATFORM_MODULE) \
                                $(QUANTA_IX8C_56X_PLATFORM_MODULE) \
                                $(QUANTA_IX9_32X_PLATFORM_MODULE) \
                                $(MITAC_LY1200_32X_PLATFORM_MODULE) \
