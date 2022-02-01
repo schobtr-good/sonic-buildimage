4bd969300487f26d3a6f318725d4fc0bb2758c6d
Wirut Getbamrung <wgetbum@icloud.com>
2022-02-01 23:22:27 +0700

    [platform/cel-mrvl_cpo1]: init platform api

---
+++ device/celestica/x86_64-cel_marvell_tl7-r0/sonic_platform/chassis.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+#############################################################################
+# Celestica
+#
+# Module contains an implementation of SONiC Platform Base API and
+# provides the Chassis information which are available in the platform
+#
+#############################################################################
+
+try:
+    import sys
+    import time
+    from sonic_platform_base.chassis_base import ChassisBase
+    from helper import APIHelper
+except ImportError as e:
+    raise ImportError(str(e) + "- required module not found")
+
+NUM_COMPONENT = 8
+
+RESET_REGISTER = "0xA106"
+HOST_REBOOT_CAUSE_PATH = "/host/reboot-cause/"
+REBOOT_CAUSE_FILE = "reboot-cause.txt"
+GETREG_PATH = "/sys/devices/platform/ctrl_cpld/getreg"
+
+
+class Chassis(ChassisBase):
+    """Platform-specific Chassis class"""
+
+    def __init__(self):
+        ChassisBase.__init__(self)
+
+        self._api_helper = APIHelper()
+        self._is_host = self._api_helper.is_host()
+
+        self._initialize_eeprom()
+        self._initialize_components()
+
+    def _initialize_eeprom(self):
+        from sonic_platform.eeprom import Tlv
+        self._eeprom = Tlv()
+
+    def _initialize_components(self):
+        from sonic_platform.component import Component
+        for index in range(0, NUM_COMPONENT):
+            component = Component(index)
+            self._component_list.append(component)
+
+    def get_base_mac(self):
+        """
+        Retrieves the base MAC address for the chassis
+        Returns:
+            A string containing the MAC address in the format
+            'XX:XX:XX:XX:XX:XX'
+        """
+        return self._eeprom.get_mac()
+
+    def get_serial_number(self):
+        """
+        Retrieves the hardware serial number for the chassis
+        Returns:
+            A string containing the hardware serial number for this chassis.
+        """
+        return self._eeprom.get_serial()
+
+    def get_system_eeprom_info(self):
+        """
+        Retrieves the full content of system EEPROM information for the chassis
+        Returns:
+            A dictionary where keys are the type code defined in
+            OCP ONIE TlvInfo EEPROM format and values are their corresponding
+            values.
+        """
+        return self._eeprom.get_eeprom()
+
+    def get_reboot_cause(self):
+        """
+        Retrieves the cause of the previous reboot
+        Returns:
+            A tuple (string, string) where the first element is a string
+            containing the cause of the previous reboot. This string must be
+            one of the predefined strings in this class. If the first string
+            is "REBOOT_CAUSE_HARDWARE_OTHER", the second string can be used
+            to pass a description of the reboot cause.
+
+            REBOOT_CAUSE_POWER_LOSS = "Power Loss"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_CPU = "Thermal Overload: CPU"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC = "Thermal Overload: ASIC"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_OTHER = "Thermal Overload: Other"
+            REBOOT_CAUSE_INSUFFICIENT_FAN_SPEED = "Insufficient Fan Speed"
+            REBOOT_CAUSE_WATCHDOG = "Watchdog"
+            REBOOT_CAUSE_HARDWARE_OTHER = "Hardware - Other"
+            REBOOT_CAUSE_NON_HARDWARE = "Non-Hardware"
+
+        """
+        reboot_cause_path = (HOST_REBOOT_CAUSE_PATH + REBOOT_CAUSE_FILE)
+        sw_reboot_cause = self._api_helper.read_txt_file(
+            reboot_cause_path) or "Unknown"
+        hw_reboot_cause = self._api_helper.get_cpld_reg_value(
+            GETREG_PATH, RESET_REGISTER)
+
+        prev_reboot_cause = {
+            '0x11': (self.REBOOT_CAUSE_POWER_LOSS, "The last reset is Power on reset"),
+            '0x22': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is soft-set CPU warm reset"),
+            '0x33': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is soft-set CPU cold reset"),
+            '0x55': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is CPU cold reset"),
+            '0x66': (self.REBOOT_CAUSE_WATCHDOG, "The last reset is gpio watchdog reset.t"),
+            '0x77': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is power cycle reset"),
+            '0x88': (self.REBOOT_CAUSE_WATCHDOG, "The last reset is hw watchdog reset")
+
+        }.get(hw_reboot_cause, (self.REBOOT_CAUSE_HARDWARE_OTHER, 'Unknown reason'))
+
+        if sw_reboot_cause != 'Unknown':
+            prev_reboot_cause = (
+                self.REBOOT_CAUSE_NON_HARDWARE, sw_reboot_cause)
+
+        return prev_reboot_cause
+
+    ##############################################################
+    ###################### Device methods ########################
+    ##############################################################
+
+    def get_name(self):
+        """
+        Retrieves the name of the device
+            Returns:
+            string: The name of the device
+        """
+        return self._api_helper.hwsku
+
+    def get_presence(self):
+        """
+        Retrieves the presence of the Chassis
+        Returns:
+            bool: True if Chassis is present, False if not
+        """
+
+        return True
+
+    def get_model(self):
+        """
+        Retrieves the model number (or part number) of the device
+        Returns:
+            string: Model/part number of device
+        """
+        return self._eeprom.get_pn()
+
+    def get_serial(self):
+        """
+        Retrieves the serial number of the device
+        Returns:
+            string: Serial number of device
+        """
+        return self.get_serial_number()
+
+    def get_status(self):
+        """
+        Retrieves the operational status of the device
+        Returns:
+            A boolean value, True if device is operating properly, False if not
+        """
+        return True
---
+++ device/celestica/x86_64-cel_marvell_tl7-r0/sonic_platform/component.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+#############################################################################
+# Celestica
+#
+# Component contains an implementation of SONiC Platform Base API and
+# provides the components firmware management function
+#
+#############################################################################
+
+import os.path
+import shutil
+import subprocess
+import time
+
+try:
+    from sonic_platform_base.component_base import ComponentBase
+    from helper import APIHelper
+except ImportError as e:
+    raise ImportError(str(e) + "- required module not found")
+
+FPGA_VERSION_PATH = "/sys/devices/platform/fpga-sys/version"
+CTRL_CPLD_VERSION_PATH = "/sys/devices/platform/ctrl_cpld/version"
+COME_CPLD_VERSION_PATH = "/sys/devices/platform/come_cpld/version"
+BIOS_VERSION_PATH = "/sys/class/dmi/id/bios_version"
+BMC_VERSION_CMD = "ipmitool raw 0x32 0x8f 0x08 0x01"
+FAN_CPLD_VERSION_CMD = "ipmitool raw 0x3a 0x64 02 01 00"
+COMPONENT_NAME_LIST = ["BMC", "BIOS", "CTRL_CPLD", "COME_CPLD", "FAN_CPLD",
+                       "LED_CPLD", "BASE_FPGA", "SW_FPGA"]
+COMPONENT_DES_LIST = ["BMC", "BIOS", "CTRL_CPLD", "COME_CPLD", "FAN_CPLD",
+                      "LED_CPLD", "BASE_FPGA", "SW_FPGA"]
+
+
+class Component(ComponentBase):
+    """Platform-specific Component class"""
+
+    DEVICE_TYPE = "component"
+
+    def __init__(self, component_index):
+        ComponentBase.__init__(self)
+        self.index = component_index
+        self._api_helper = APIHelper()
+        self.name = self.get_name()
+
+    def __get_bios_version(self):
+        # Retrieves the BIOS firmware version
+        status, result = self._api_helper.run_command(
+            "ipmitool raw 0x3a 0x64 00 01 0x70")
+        if result.strip() == "01":
+            if self.name == "Main_BIOS":
+                with open(BIOS_VERSION_PATH, 'r') as fd:
+                    bios_version = fd.read()
+                    return bios_version.strip()
+            elif self.name == "Backup_BIOS":
+                bios_version = "na"
+                return bios_version
+
+        elif result.strip() == "03":
+            if self.name == "Backup_BIOS":
+                with open(BIOS_VERSION_PATH, 'r') as fd:
+                    bios_version = fd.read()
+                    return bios_version.strip()
+            elif self.name == "Main_BIOS":
+                bios_version = "na"
+                return bios_version
+
+    def get_register_value(self, register):
+        # Retrieves the cpld register value
+        cmd = "echo {1} > {0}; cat {0}".format(GETREG_PATH, register)
+        p = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        raw_data, err = p.communicate()
+        if err is not '':
+            return None
+        return raw_data.strip()
+
+    def __get_cpld_version(self):
+        if self.name == "CTRL_CPLD":
+            try:
+                with open(CTRL_CPLD_VERSION_PATH, 'r') as fd:
+                    CTRL_CPLD_version = fd.read()
+                    return CTRL_CPLD_version.strip()
+            except Exception as e:
+                return None
+        elif self.name == "COME_CPLD":
+            try:
+                with open(COME_CPLD_VERSION_PATH, 'r') as fd:
+                    bcpld_version = fd.read()
+                    return bcpld_version.strip()
+            except Exception as e:
+                return None
+        elif self.name == "FAN_CPLD":
+            status, ver = self._api_helper.run_command(FAN_CPLD_VERSION_CMD)
+            version = int(ver.strip(), 16)
+            return str(version)
+
+    def __get_fpga_version(self):
+        # Retrieves the FPGA firmware version
+        try:
+            with open(FPGA_VERSION_PATH, 'r') as fd:
+                version = fd.read()
+                fpga_version = (version.strip().split("x")[1])
+                return fpga_version.strip()
+        except Exception as e:
+            return None
+
+    def __get_bmc_version(self):
+        # Retrieves the BMC firmware version
+        status, ver = self._api_helper.run_command(BMC_VERSION_CMD)
+        return ver.strip()
+
+    def get_name(self):
+        """
+        Retrieves the name of the component
+         Returns:
+            A string containing the name of the component
+        """
+        return COMPONENT_NAME_LIST[self.index]
+
+    def get_description(self):
+        """
+        Retrieves the description of the component
+            Returns:
+            A string containing the description of the component
+        """
+        return COMPONENT_DES_LIST[self.index]
+
+    def get_firmware_version(self):
+        """
+        Retrieves the firmware version of module
+        Returns:
+            string: The firmware versions of the module
+        """
+        fw_version = "Unknown"
+
+        if "BIOS" in self.name:
+            fw_version = self.__get_bios_version()
+        elif "CPLD" in self.name:
+            fw_version = self.__get_cpld_version()
+        elif self.name == "BASE_FPGA":
+            fw_version = self.__get_fpga_version()
+        elif "BMC" in self.name:
+            version = self.__get_bmc_version()
+            version_1 = int(version.strip().split(" ")[0], 16)
+            version_2 = int(version.strip().split(" ")[1], 16)
+            fw_version = "%s.%s" % (version_1, version_2)
+
+        return fw_version
+
+    def get_available_firmware_version(self, image_path):
+        """
+        Retrieves the available firmware version of the component
+        Note: the firmware version will be read from image
+        Args:
+            image_path: A string, path to firmware image
+        Returns:
+            A string containing the available firmware version of the component
+        """
+        return "N/A"
+
+    def install_firmware(self, image_path):
+        """
+        Installs firmware to the component
+        This API performs firmware installation only: this may/may not be the same as firmware update.
+        In case platform component requires some extra steps (apart from calling Low Level Utility)
+        to load the installed firmware (e.g, reboot, power cycle, etc.) - this must be done manually by user
+        Note: in case immediate actions are required to complete the component firmware update
+        (e.g., reboot, power cycle, etc.) - will be done automatically by API and no return value provided
+        Args:
+            image_path: A string, path to firmware image
+        Returns:
+            A boolean, True if install was successful, False if not
+        """
+        return False
+
+    def update_firmware(self, image_path):
+        """
+        Updates firmware of the component
+        This API performs firmware update: it assumes firmware installation and loading in a single call.
+        In case platform component requires some extra steps (apart from calling Low Level Utility)
+        to load the installed firmware (e.g, reboot, power cycle, etc.) - this will be done automatically by API
+        Args:
+            image_path: A string, path to firmware image
+        Raises:
+            RuntimeError: update failed
+        """
+        raise RuntimeError
+
+    def auto_update_firmware(self, image_path, boot_type):
+        """
+        Updates firmware of the component
+        This API performs firmware update automatically based on boot_type: it assumes firmware installation
+        and/or creating a loading task during the reboot, if needed, in a single call.
+        In case platform component requires some extra steps (apart from calling Low Level Utility)
+        to load the installed firmware (e.g, reboot, power cycle, etc.) - this will be done automatically during the reboot.
+        The loading task will be created by API.
+        Args:
+            image_path: A string, path to firmware image
+            boot_type: A string, reboot type following the upgrade
+                         - none/fast/warm/cold
+        Returns:
+            Output: A return code
+                return_code: An integer number, status of component firmware auto-update
+                    - return code of a positive number indicates successful auto-update
+                        - status_installed = 1
+                        - status_updated = 2
+                        - status_scheduled = 3
+                    - return_code of a negative number indicates failed auto-update
+                        - status_err_boot_type = -1
+                        - status_err_image = -2
+                        - status_err_unknown = -3
+        Raises:
+            RuntimeError: auto-update failure cause
+        """
+        raise RuntimeError
