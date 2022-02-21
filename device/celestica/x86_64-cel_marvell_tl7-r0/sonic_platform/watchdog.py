fe91bbba51f99c265c2a378bd5fec826e88c619e
Wirut Getbamrung <wgetbum@icloud.com>
2022-02-21 17:38:25 +0700

    [device/celestica]: fix watchdog apis
    * [platform/innovium-cel]: fix mrvl platform_sensors.py permission
    
    * [device/celestica]: fix watchdog apis

--- device/celestica/x86_64-cel_marvell_tl7-r0/sonic_platform/watchdog.py
+++ device/celestica/x86_64-cel_marvell_tl7-r0/sonic_platform/watchdog.py
@@ -4,10 +4,11 @@
 # Watchdog contains an implementation of SONiC Platform Base API
 #
 #############################################################################
-import os
-import time
 
 try:
+    import os
+    import time
+    import ctypes
     from sonic_platform_base.watchdog_base import WatchdogBase
     from helper import APIHelper
 except ImportError as e:
@@ -22,6 +23,16 @@ WDT_KEEP_ALVIVE_REG = '0xA184'
 ENABLE_CMD = '0x1'
 DISABLE_CMD = '0x0'
 WDT_COMMON_ERROR = -1
+CLOCK_MONOTONIC = 1
+
+ARMED_TIME_FILE = "/tmp/wdt_armed_time"
+
+
+class _timespec(ctypes.Structure):
+    _fields_ = [
+        ('tv_sec', ctypes.c_long),
+        ('tv_nsec', ctypes.c_long)
+    ]
 
 
 class Watchdog(WatchdogBase):
@@ -34,9 +45,19 @@ class Watchdog(WatchdogBase):
         self.getreg_path = os.path.join(PLATFORM_CPLD_PATH, GETREG_FILE)
 
         # Set default value
-        self._disable()
-        self.armed = False
         self.timeout = self._gettimeout()
+        self._set_armed_time_file()
+
+        self._librt = ctypes.CDLL('librt.so.1', use_errno=True)
+        self._clock_gettime = self._librt.clock_gettime
+        self._clock_gettime.argtypes = [
+            ctypes.c_int, ctypes.POINTER(_timespec)]
+
+    def _set_armed_time_file(self):
+        if not os.path.exists(ARMED_TIME_FILE):
+            f = open(ARMED_TIME_FILE, 'w')
+            f.write(0)
+            f.close()
 
     def _enable(self):
         """
@@ -77,9 +98,7 @@ class Watchdog(WatchdogBase):
             420: 6,
             600: 7,
         }.get(seconds)
-
-        self._api_helper.write_txt_file(self.setreg_path, second_int)
-        return seconds
+        return self._api_helper.write_txt_file(self.setreg_path, second_int), seconds
 
     def _gettimeout(self):
         """
@@ -89,7 +108,6 @@ class Watchdog(WatchdogBase):
         hex_str = self._api_helper.get_cpld_reg_value(
             self.getreg_path, WDT_TIMER_REG)
         bin_val = bin(int(hex_str, 16))[2:].zfill(3)
-
         return {
             '001': 30,
             '010': 60,
@@ -99,6 +117,16 @@ class Watchdog(WatchdogBase):
             '110': 420,
             '111': 600
         }.get(bin_val)
+
+    def _get_time(self):
+        """
+        To get clock monotonic time
+        """
+        ts = _timespec()
+        if self._clock_gettime(CLOCK_MONOTONIC, ctypes.pointer(ts)) != 0:
+            self._errno = ctypes.get_errno()
+            return 0
+        return ts.tv_sec + ts.tv_nsec * 1e-9
 
     #################################################################
 
@@ -117,22 +145,28 @@ class Watchdog(WatchdogBase):
 
         avaliable_second = [30, 60, 180, 240, 300, 420, 600]
         ret = WDT_COMMON_ERROR
+        status = False
 
         if seconds < 0 or seconds not in avaliable_second:
+            print("The second input is invalid, available options are:")
+            print(avaliable_second)
             return ret
 
         try:
             if self.timeout != seconds:
-                self.timeout = self._settimeout(seconds)
+                status, self.timeout = self._settimeout(seconds)
 
-            if self.armed:
+                if not status:
+                    return ret
+
+            if self.is_armed():
                 self._keepalive()
             else:
                 self._enable()
-                self.armed = True
 
             ret = self.timeout
-            self.arm_timestamp = time.time()
+            self._api_helper.write_txt_file(
+                ARMED_TIME_FILE, str(self._get_time()))
         except IOError as e:
             print(e)
             pass
@@ -149,9 +183,11 @@ class Watchdog(WatchdogBase):
         if self.is_armed():
             try:
                 self._disable()
-                self.armed = False
                 disarmed = True
-            except IOError:
+                self._api_helper.write_txt_file(
+                    ARMED_TIME_FILE, 0)
+            except IOError as e:
+                print(e)
                 pass
 
         return disarmed
@@ -162,8 +198,9 @@ class Watchdog(WatchdogBase):
         Returns:
             A boolean, True if watchdog is armed, False if not
         """
-
-        return self.armed
+        hex_str = self._api_helper.get_cpld_reg_value(
+            self.getreg_path, WDT_ENABLE_REG)
+        return int(hex_str, 16) == int(ENABLE_CMD, 16)
 
     def get_remaining_time(self):
         """
@@ -174,4 +211,23 @@ class Watchdog(WatchdogBase):
             watchdog timer. If the watchdog is not armed, returns -1.
         """
 
-        return int(self.timeout - (time.time() - self.arm_timestamp)) if self.armed else WDT_COMMON_ERROR
+        if not self.is_armed():
+            return -1
+
+        armed_time = float(
+            self._api_helper.read_one_line_file(ARMED_TIME_FILE))
+
+        if armed_time > 0 and self.timeout != 0:
+            cur_time = self._get_time()
+
+            if cur_time <= 0:
+                return 0
+
+            diff_time = int(cur_time - armed_time)
+
+            if diff_time > self.timeout:
+                return self.timeout
+            else:
+                return self.timeout - diff_time
+
+        return 0
